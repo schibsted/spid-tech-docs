a24ec31c35d77778cde36efbb252b390385ae8ab
Christian Johansen <christian@cjohansen.no>
2014-02-19 14:33:57 +0100

    List HTTP methods and optional/required parameters

--- src/spid_docs/web.clj
+++ src/spid_docs/web.clj
@@ -34,6 +34,21 @@
                             [:br]
                             (:description %))) (:data endpoints))])))
 
+(defn format-name [format]
+  (cond
+   (= "json" format) "JSON"
+   (= "jsonp" format) "JSON-P"))
+
+(defn- render-params [heading params]
+  (if (seq params)
+    (list [:h3 heading]
+          [:ul (map #(vector :li %) params)])))
+
+(defn- render-http-methods [methods]
+  (mapcat #(list [:h2 (:name %)]
+                 (render-params "Required params" (:required %))
+                 (render-params "Optional params" (:optional %))) (vals methods)))
+
 (defn render-page [endpoint context]
   (layout/page
    context
@@ -43,10 +58,11 @@
          [:table
           [:tr [:th "Requires authentication"] [:td "?"]]
           [:tr [:th "Supported access token types"] [:td "?"]]
-          [:tr [:th "Supported response format"] [:td (str/join ", " (:valid_output_formats endpoint))]]
+          [:tr [:th "Supported response format"] [:td (str/join ", " (map format-name (:valid_output_formats endpoint)))]]
           [:tr [:th "Supported filters"] [:td "?"]]
           [:tr [:th "Default filters"] [:td "?"]]
           [:tr [:th "Successful return"] [:td "?"]]]
+         (render-http-methods (:httpMethods endpoint))
          [:pre (with-out-str (clojure.pprint/pprint endpoint))])))
 
 (defn get-endpoint-pages [endpoints]
