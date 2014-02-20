9797c57f91c38735d16b8c78494fd4356ed560ff
Christian Johansen <christian@cjohansen.no>
2014-02-20 15:28:56 +0100

    Rendre mer data fra endpoint-datastruktur

--- src/spid_docs/endpoints.clj
+++ src/spid_docs/endpoints.clj
@@ -21,6 +21,50 @@
    (= "json" format) "JSON"
    (= "jsonp" format) "JSON-P"))
 
+(defn- render-key-properties [endpoint]
+  [:table.boxed.zebra
+   [:tr [:th "Requires authentication"] [:td (if (:auth-required endpoint) "Yes" "No")]]
+   (if-let [token-types (:access-token-types endpoint)]
+     [:tr
+      [:th "Supported access token types"]
+      [:td token-types]])
+   [:tr
+    [:th "Supported response format"]
+    [:td (str/join ", " (map format-name (:valid_output_formats endpoint)))]]
+   (let [filters (:filters endpoint)]
+     [:tr
+      [:th (str "Supported filter" (if (= (count filters) 1) "" "s"))]
+      [:td (if (seq filters) (str/join ", " filters) "None")]])
+   (if-let [filter (:default-filters endpoint)]
+     [:tr [:th "Default filters"] [:td filter]])
+   [:tr [:th "Successful return"] [:td "200"]]])
+
+(defn- render-object [type]
+  (list [:h2 (:name type)]
+        (if-let [description (:description type)] [:p description])
+        [:table.boxed.zebra
+         [:tr [:th "Field"] [:th "Type"] [:th "Description"]]
+         (map #(vector :tr
+                       [:th (:field %)]
+                       [:td (name (:type %))]
+                       [:td (:description %)])
+              (:fields type))]))
+
+(defn- render-string [type]
+  (list [:h2 {:id (name (:id type))} (:name type)]
+        (if-let [description (:description type)] [:p description])
+        [:table.boxed.zebra
+         [:tr [:th "Value"] [:th "Description"]]
+         (map #(vector :tr
+                       [:th (:value %)]
+                       [:td (:description %)])
+              (:values type))]))
+
+(defn- render-types [endpoint]
+  (map #(if (= (:type %) :object)
+          (render-object %)
+          (render-string %)) (:types endpoint)))
+
 (defn endpoint-url [endpoint]
   (str "/" (:path endpoint)))
 
@@ -31,23 +75,9 @@
   {:title (endpoint-url endpoint)
    :body (list [:h1 (:name endpoint)]
                [:p (:description endpoint)]
-               [:table.boxed.zebra
-                [:tr [:th "Requires authentication"] [:td (if (:auth-required endpoint) "Yes" "No")]]
-                (if-let [token-types (:access-token-types endpoint)]
-                  [:tr
-                   [:th "Supported access token types"]
-                   [:td token-types]])
-                [:tr
-                 [:th "Supported response format"]
-                 [:td (str/join ", " (map format-name (:valid_output_formats endpoint)))]]
-                (let [filters (:filters endpoint)]
-                  [:tr
-                   [:th (str "Supported filter" (if (= (count filters) 1) "" "s"))]
-                   [:td (if (seq filters) (str/join ", " filters) "None")]])
-                (if-let [filter (:default-filters endpoint)]
-                  [:tr [:th "Default filters"] [:td filter]])
-                [:tr [:th "Successful return"] [:td "200"]]]
+               (render-key-properties endpoint)
                (render-http-methods endpoint)
+               (render-types endpoint)
                [:pre (with-out-str (pprint endpoint))])})
 
 (defn create-pages [endpoints]
