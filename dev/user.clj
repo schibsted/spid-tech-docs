206acc657f43c52109bdff47bb1d74f4a5d99d13
Christian Johansen <christian@cjohansen.no>
2014-02-25 09:51:57 +0100

    Beef up the repl experience

---
+++ dev/user.clj
@@ -0,0 +1,7 @@
+(ns user
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]
+            [clojure.pprint :refer [pprint pp print-table]]
+            [clojure.repl :refer :all]
+            [hiccup.core :as hiccup]))
