a3ec65f4ced00e95dd04faa2bb01222040c6df82
Magnar Sveen <magnars@gmail.com>
2014-03-04 19:55:52 +0100

    Responsive Design

--- src/spid_docs/layout.clj
+++ src/spid_docs/layout.clj
@@ -7,8 +7,10 @@
   (html5
    [:head
     [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1.0"}]
     [:title (str title " | SPiD API Documentation")]
-    [:link {:rel "stylesheet" :type "text/css" :href (link/file-path request "/styles/spid.css")}]]
+    [:link {:rel "stylesheet" :type "text/css" :href (link/file-path request "/styles/responsive.css")}]]
    [:body
     [:script "document.body.className = 'js';"]
     [:div#main
