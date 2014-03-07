47b08e5e68f76d3cd48b0d70a20f187a76297259
Christian Johansen <christian@cjohansen.no>
2014-03-07 11:08:37 +0100

    Turn enlive transform into a macro
    Accepts arbitrary arguments

--- src/spid_docs/enlive.clj
+++ src/spid_docs/enlive.clj
@@ -1,19 +1,24 @@
 (ns spid-docs.enlive
+  "Enlive is a templating toolkit that offer very cool things like document
+   transformations based on selections. Unfortunately, it's API is notoriously
+   hard to work with, so this little wrapper presents the things we use in a
+   more digestable way."
   (:require [hiccup.core :as hiccup]
             [net.cgrand.enlive-html :as enlive])
   (:import java.io.StringReader))
 
-(defn parse [data]
+(defn parse
+  "Parse a string of HTML into a data structure enlive can work with"
+  [data]
   (if (string? data)
     (enlive/html-snippet data)
     (enlive/html-snippet (hiccup/html data))))
 
-(defn transform
-  "TODO: Make this a macro that accepts any number of forms.
-   Must be a macro, because pretty much all of enlive is a macro,
-   thus cannot be apply-ed"
-  [tree selector fn]
-  (enlive/sniptest* tree (enlive/transformation selector fn)))
+(defmacro transform
+  "Accepts an enlive data structure or a string of HTML followed by pairs of
+   selectors and transformations. Returns the transformed markup as a string."
+  [tree & forms]
+  `(enlive/sniptest* (if (string? ~tree) (parse ~tree) ~tree) (enlive/transformation ~@forms)))
 
 (defn select [selector tree]
   (enlive/select tree selector))
