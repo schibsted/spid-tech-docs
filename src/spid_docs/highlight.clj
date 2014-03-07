e9bfe00b93221ac54b1daee1f930edc8996ba857
Magnar Sveen <magnars@gmail.com>
2014-03-07 07:24:50 +0100

    Write some docstrings for highlight.clj

--- src/spid_docs/highlight.clj
+++ src/spid_docs/highlight.clj
@@ -1,8 +1,11 @@
 (ns spid-docs.highlight
+  "Use Pygments to highlight code examples."
   (:require [net.cgrand.enlive-html :refer [sniptest html-resource select]]
             [clygments.core :as pygments]))
 
 (defn- extract-code [highlighted]
+  "Pulls out just the highlighted code, removing needless fluff and
+   stuff from the Pygments treatment."
   (-> highlighted
       java.io.StringReader.
       html-resource
@@ -11,6 +14,8 @@
       :content))
 
 (defn highlight [node]
+  "Extracts code from the node contents, and highlights it according
+   to the given language (extracted from the node's class name)."
   (let [code (->> node :content (apply str))
         lang (->> node :attrs :class keyword)]
     {:tag :code
@@ -19,6 +24,8 @@
                   (extract-code))}))
 
 (defn highlight-code-blocks [markup]
+  "Finds all <pre> with <code> in them and highlights with Pygments."
   (sniptest markup
             [:pre :code] highlight
-            [:pre] #(assoc-in % [:attrs :class] "codehilite")))
+            [:pre] #(assoc-in % [:attrs :class] "codehilite"))) ;; fixme: this matches too many <pre>'s
+
