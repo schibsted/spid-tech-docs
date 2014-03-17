93ea34e4b7a62be24eba436e1bd523700d5183fe
Magnar Sveen <magnars@gmail.com>
2014-03-17 15:04:39 +0100

    New cultivate taking shape

--- src/spid_docs/cultivate/new_endpoints.clj
+++ src/spid_docs/cultivate/new_endpoints.clj
@@ -6,8 +6,18 @@
       (str/replace #"[^a-zA-Z]+" "-")
       (str/replace #"-$" "")))
 
-(defn- cultivate-endpoint-1 [endpoint method]
-  [{:id (-> endpoint :path generate-id)}])
+(def ^:private verbs {:GET "Get"
+                      :POST "Create"
+                      :DELETE "Delete"})
+
+(defn- fix-multimethod-name [name method]
+  (str/replace name #"(?i)^(?:(?:get|create|delete),? )+or (?:get|create|delete) " (str (verbs method) " ")))
+
+(defn- cultivate-endpoint-1 [endpoint [method details]]
+  {:id (-> (:path endpoint) generate-id)
+   :path (str "/" (:path endpoint))
+   :method method
+   :name (fix-multimethod-name (:name endpoint) method)})
 
 (defn cultivate-endpoint [endpoint]
-  (mapcat (partial cultivate-endpoint-1 endpoint) (:httpMethods endpoint)))
+  (map (partial cultivate-endpoint-1 endpoint) (:httpMethods endpoint)))
--- test/spid_docs/cultivate/new_endpoints_test.clj
+++ test/spid_docs/cultivate/new_endpoints_test.clj
@@ -3,19 +3,40 @@
             [spid-docs.cultivate.content-shells :as cs]
             [spid-docs.cultivate.new-endpoints :refer :all]))
 
-(fact (-> (cs/endpoint :path "terms") cultivate-endpoint first :id) => "terms")
-(fact (-> (cs/endpoint :path "describe/{object}") cultivate-endpoint first :id) => "describe-object")
-
 (fact
  "Number of returned endpoints depends on the given http methods."
 
- (-> (cs/endpoint :httpMethods {})
-     cultivate-endpoint count) => 0
-
- (-> (cs/endpoint :httpMethods {:GET (cs/http-method)})
-     cultivate-endpoint count) => 1
-
+ (-> (cs/endpoint :httpMethods {}) cultivate-endpoint count) => 0
+ (-> (cs/endpoint :httpMethods {:GET (cs/http-method)}) cultivate-endpoint count) => 1
  (-> (cs/endpoint :httpMethods {:GET (cs/http-method)
-                                :POST (cs/http-method)})
-     cultivate-endpoint count) => 2)
+                                :POST (cs/http-method)}) cultivate-endpoint count) => 2)
 
+(fact (-> (cs/endpoint :path "terms") cultivate-endpoint first :id) => "terms")
+(fact (-> (cs/endpoint :path "describe/{object}") cultivate-endpoint first :id) => "describe-object")
+
+(fact (-> (cs/endpoint :path "terms") cultivate-endpoint first :path) => "/terms")
+
+(fact (-> (cs/endpoint :httpMethods {:GET (cs/http-method)}) cultivate-endpoint
+          first :method) => :GET)
+
+(fact
+ "Names are cut apart and stiched together to fit the httpMethod."
+
+ (-> (cs/endpoint :name "Create subscription") cultivate-endpoint first :name)
+ => "Create subscription"
+
+ (->> (cs/endpoint :name "Get or create subscription"
+                   :httpMethods {:GET (cs/http-method)
+                                 :POST (cs/http-method)})
+      cultivate-endpoint (map (juxt :method :name)) set)
+ => #{[:GET "Get subscription"]
+      [:POST "Create subscription"]}
+
+ (->> (cs/endpoint :name "Create, get or delete subscription"
+                   :httpMethods {:GET (cs/http-method)
+                                 :POST (cs/http-method)
+                                 :DELETE (cs/http-method)})
+      cultivate-endpoint (map (juxt :method :name)) set)
+ => #{[:GET "Get subscription"]
+      [:POST "Create subscription"]
+      [:DELETE "Delete subscription"]})
