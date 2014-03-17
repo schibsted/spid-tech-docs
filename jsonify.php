796c14dd21445bbfa87e033379ef2b27804d44c4
Christian Johansen <christian@cjohansen.no>
2014-03-17 09:05:32 +0100

    Produce mockup endpoints JSON

---
+++ jsonify.php
@@ -0,0 +1,33 @@
+<?php
+require("routes.api.2.php");
+
+echo json_encode(array(
+    "name" => "SPP Container",
+    "version" => "0.2",
+    "api" => 2,
+    "object" => "Utility",
+    "type" => "collection",
+    "code" => 200,
+    "request" => array(
+        "reset" => 3154,
+        "limit" => 360000,
+        "remaining" => 359997
+    ),
+    "debug" => array(
+        "route" => array(
+            "name" => "List API endpoints",
+            "url" => "\/api\/2\/endpoints",
+            "controller" => "Api\/2\/Utility.endpoints"
+        ),
+        "params" => array(
+            "options" => array(),
+            "where" => array()
+        )
+    ),
+    "meta" => array(
+        "count" => 98,
+        "offset" => 0
+    ),
+    "error" => null,
+    "data" => $_CORE_ROUTES
+));
