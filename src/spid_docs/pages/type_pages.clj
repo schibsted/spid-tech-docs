60741f60f2e6daffaded48541d826dc7e8618285
Christian Johansen <christian@cjohansen.no>
2014-03-19 23:43:20 +0100

    Properly render typedefs

--- src/spid_docs/pages/type_pages.clj
+++ src/spid_docs/pages/type_pages.clj
@@ -5,14 +5,31 @@
 (defn- render-availability [field]
   (if (:always-available? field) [:span.check "✓"]))
 
-(defn- link-to-type [type types]
-  (let [typedef (type types)
+(defn- link-to-type
+  "Link to the type passed in. The type should be either a keyword, which names
+   the type, or it may be a vector. If it is a vector, it should contain a
+   single keyword, and denotes a list of the type named by the keyword.
+
+   The types map defines possible types to link to. If the type is a link to a
+   type with a description, a link will be generated. If the target type has no
+   description, it is considered trivial, and no link is offered, only the type
+   name. If the target type is pertinent, it is assumed rendered on the same
+   page, and an anchor link is generated."
+  [type types]
+  (let [list-type? (vector? type)
+        type-id (if list-type? (first type) type)
+        type-name (name type-id)
+        prefix (if list-type? "list of " "")
+        postfix (if list-type? (if (.endsWith type-name "s") "es" "s") "")
+        typedef (type-id types)
         path (if (:pertinent? typedef)
                (str "#" (name type))
                (type-path typedef))]
-    (if path
-      [:a {:href path} type]
-      type)))
+    (list prefix
+          (if path
+            [:a {:href path} type-name]
+            type-name)
+          postfix)))
 
 (defn- render-type-field [field types]
   [:tr
--- test/spid_docs/pages/types_pages_test.clj
+++ test/spid_docs/pages/types_pages_test.clj
@@ -1,6 +1,7 @@
 (ns spid-docs.pages.types-pages-test
-  (:require [spid-docs.pages.type-pages :refer :all]
-            [midje.sweet :refer :all]))
+  (:require [hiccup-find.core :refer [hiccup-find hiccup-text]]
+            [midje.sweet :refer :all]
+            [spid-docs.pages.type-pages :refer :all]))
 
 (fact "It creates pages for types with description"
       (let [pages (create-pages {:string {:id :string}
@@ -10,3 +11,27 @@
         ((pages "/types/datetime")) => {:body [:div.wrap
                                                [:h1 "datetime"]
                                                "<p>ISO-8601</p>"]}))
+
+(let [login {:id :login
+             :name "Login"
+             :description "A login object"
+             :fields [{:name "id" :type :string}
+                      {:name "emails" :type [:string]}]}
+      types {:login login
+             :string {:id :string :name "String"}
+             :login-type {:id :login-type
+                          :name "Login type"
+                          :description "Something"}}]
+
+  (fact "Renders type definitions"
+        (->> (render-type-definition login types)
+             (hiccup-find [:table.sectioned])
+             count) => 1)
+
+  (fact "Links to non-pertinent types with description"
+        (->> (render-type-definition login types)
+             (hiccup-find [:h5 :a]))) => 1)
+
+
+
+;; TODO: [string]
