780d24a9e4f2833ae5f61d55e85e27a59fa94beb
Christian Johansen <christian@cjohansen.no>
2014-03-19 23:24:42 +0100

    Add routes tests

---
+++ test/spid_docs/routes_test.clj
@@ -0,0 +1,10 @@
+(ns spid-docs.routes-test
+  (:require [spid-docs.routes :refer :all]
+            [midje.sweet :refer :all]))
+
+(fact "It generates type paths only for types with descriptions"
+      (type-path {:id :string}) => nil
+      (type-path {:id :datetime :description "ISO-8601"}) => "/types/datetime")
+
+(fact "It generates endpoint paths"
+      (endpoint-path {:path "/logins" :method "GET"}) => "/endpoints/GET/logins")
