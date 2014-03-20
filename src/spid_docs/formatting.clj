ca88bcdf49a9b229a2f23e198b98c2df863f3f83
Christian Johansen <christian@cjohansen.no>
2014-03-20 10:17:06 +0100

    General purpose columnize

--- src/spid_docs/formatting.clj
+++ src/spid_docs/formatting.clj
@@ -26,3 +26,44 @@
       (str/replace #"[^a-zA-Z]+" "-")
       (str/replace #"-$" "")
       (str/replace #"^-" "")))
+
+(defn- shortest-index
+  "Given a collection, find the index where the provided count-fn function
+  returns the lowest number of items. Supporting function for columnize."
+  [coll count-fn]
+  (loop [idx -1
+         shortest (inc (apply max (map count-fn coll)))
+         i 0]
+    (if (< i (count coll))
+      (if (< (count-fn (nth coll i)) shortest)
+        (recur i (count-fn (nth coll i)) (inc i))
+        (recur idx shortest (inc i)))
+      idx)))
+
+(defn- column-count
+  "Using a specific counting function, decide how many items are present in a
+  column. Supporting function for columnize."
+  [count-fn]
+  (fn [col]
+    (reduce + (map count-fn col))))
+
+(defn columnize
+  "Breaks a list of data into +cols+ roughly equally sized columns. With only
+   two arguments, columnize takes a list of lists, and breaks it up in n columns
+   of lists, trying to balance not the number of lists in each column, but the
+   number of items in each column. Example:
+
+   (columnize [[1 2 3] [4 5] [6] [7 8] [9 10 11]] 3)
+   ;=> [[[1 2 3] [6]] [[9 10 11]] [[4 5] [7 8]]]
+
+   By providing the count-fn argument, you can decide how the item count for
+   individual items are calculated, and can thus columnize any kind of
+   data (perhaps deeply nested)"
+  [coll cols & [count-fn]]
+  (loop [columns (mapv (fn [_] []) (range cols))
+         sorted-coll (sort #(> (count-fn %1) (count-fn %2)) coll)]
+    (if (seq sorted-coll)
+      (recur (update-in columns [(shortest-index columns (column-count count-fn))]
+                        #(conj % (first sorted-coll)))
+             (rest sorted-coll))
+      columns)))
--- test/spid_docs/formatting_test.clj
+++ test/spid_docs/formatting_test.clj
@@ -19,3 +19,18 @@
       (to-id-str "/some/path") => "some-path"
       (to-id-str "/some/{path}") => "some-path"
       (to-id-str "Some sentence") => "some-sentence")
+
+(fact "Columnizes data structures"
+      ;(columnize [[1] [2] [3] [4]] 2) => [[[1] [3]] [[2] [4]]]
+      ;(columnize [[1] [2 3] [4] [5]] 2) => [[[2 3] [5]] [[1] [4]]]
+      ;(columnize [[1] [2 3] [4] [5 6 7]] 2) => [[[5 6 7] [4]] [[2 3] [1]]]
+      ;(columnize [[1 2 3] [4 5] [6] [7 8] [9 10 11]] 3) => [[[1 2 3] [6]] [[9 10 11]] [[4 5] [7 8]]]
+      (columnize [{:things [1 2 3]}
+                  {:things [4 5]}
+                  {:things [6]}
+                  {:things [7 8 9]}
+                  {:things [10]}] 2 #(count (:things %))) => [[{:things [1 2 3]}
+                                                               {:things [4 5]}],
+                                                              [{:things [7 8 9]}
+                                                               {:things [6]}
+                                                               {:things [10]}]])
