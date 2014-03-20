8366fb568fa8ada2e1e9a2483bff23ab68ec7a4e
Magnar Sveen <magnars@gmail.com>
2014-03-20 13:48:14 +0100

    Fix routes.api assoc(array())

--- jsonify.php
+++ jsonify.php
@@ -1,43 +1,39 @@
 <?php
-// Want to turn this into edn?
-//
-// (require 'clojure.data.json)
-// (spit "pretty-endpoints.edn" (with-out-str (clojure.pprint/pprint (clojure.data.json/read-str (slurp (io/resource "cached-endpoints.json")) :key-fn keyword))))
-
 require("routes.api.2.php");
 
-/* echo json_encode(array( */
-/*     "name" => "SPP Container", */
-/*     "version" => "0.2", */
-/*     "api" => 2, */
-/*     "object" => "Utility", */
-/*     "type" => "collection", */
-/*     "code" => 200, */
-/*     "request" => array( */
-/*         "reset" => 3154, */
-/*         "limit" => 360000, */
-/*         "remaining" => 359997 */
-/*     ), */
-/*     "debug" => array( */
-/*         "route" => array( */
-/*             "name" => "List API endpoints", */
-/*             "url" => "\/api\/2\/endpoints", */
-/*             "controller" => "Api\/2\/Utility.endpoints" */
-/*         ), */
-/*         "params" => array( */
-/*             "options" => array(), */
-/*             "where" => array() */
-/*         ) */
-/*     ), */
-/*     "meta" => array( */
-/*         "count" => 98, */
-/*         "offset" => 0 */
-/*     ), */
-/*     "error" => null, */
-/*     "data" => $_CORE_ROUTES */
-/* )); */
+echo json_encode(array(
+    "name" => "SPP Container",
+    "version" => "0.2",
+    "api" => 2,
+    "object" => "Utility",
+    "type" => "collection",
+    "code" => 200,
+    "request" => array(
+        "reset" => 3154,
+        "limit" => 360000,
+        "remaining" => 359997
+    ),
+    "debug" => array(
+        "route" => array(
+            "name" => "List API endpoints",
+            "url" => "\/api\/2\/endpoints",
+            "controller" => "Api\/2\/Utility.endpoints"
+        ),
+        "params" => array(
+            "options" => array(),
+            "where" => array()
+        )
+    ),
+    "meta" => array(
+        "count" => 98,
+        "offset" => 0
+    ),
+    "error" => null,
+    "data" => $_CORE_ROUTES
+));
 
 
+/* Test that EmptyAssocArray works just like arrays
 
 $obj = new EmptyAssocArray();
 
@@ -49,3 +45,4 @@ print("Count: " . count($obj));
 print("Access: " . $obj["something"]);
 
 print(json_encode($obj));
+*/
