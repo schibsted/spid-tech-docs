daef56af2faed0f14a3bbc1eba0cc84d8d8e3d04
Christian Johansen <christian@cjohansen.no>
2014-03-21 08:24:32 +0100

    Add rudimentary link checker

--- test/spid_docs/web_test.clj
+++ test/spid_docs/web_test.clj
@@ -1,12 +1,32 @@
 (ns spid-docs.web-test
-  (:require [spid-docs.web :refer :all]
-            [midje.sweet :refer :all]))
+  (:require [clojure.string :as str]
+            [midje.sweet :refer :all]
+            [net.cgrand.enlive-html :as enlive]
+            [spid-docs.web :refer :all]))
+
+(defn link-valid? [pages href]
+  (let [path (first (str/split href #"#"))]
+    (or
+     (not (.startsWith href "/"))
+     (contains? pages href)
+     (contains? pages (str href "index.html")))))
 
 (fact
  "All pages respond with 200 OK"
 
- (doseq [url (keys (get-pages))]
-   (let [status (:status (app {:uri url}))]
-     ;; This weird comparison is made in order for Midje to show us
-     ;; *which* URL fails.
-     [url status] => [url 200])))
+ (let [pages (get-pages)]
+   (doseq [url (keys pages)]
+     (let [response (app {:uri url})
+           status (:status response)]
+       ;; This weird comparison is made in order for Midje to show us
+       ;; *which* URL fails.
+       [url status] => [url 200]
+
+       ;; Link checker
+       ;; (doseq [link (-> (:body (app {:uri url}))
+       ;;                  java.io.StringReader.
+       ;;                  enlive/html-resource
+       ;;                  (enlive/select [:a]))]
+       ;;   (let [href (get-in link [:attrs :href])]
+       ;;     [url href (link-valid? pages href)] => [url href true]))
+       ))))
