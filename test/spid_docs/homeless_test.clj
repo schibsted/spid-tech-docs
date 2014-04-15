33275d817616eda6dc57c0731b6598eec6ff37af
Christian Johansen <christian@cjohansen.no>
2014-04-15 13:57:48 +0200

    Fix homeless test
    Previous one relied on a "hot" environment

--- test/spid_docs/homeless_test.clj
+++ test/spid_docs/homeless_test.clj
@@ -1,9 +1,10 @@
 (ns spid-docs.homeless-test
-  (:require [spid-docs.homeless :refer :all]
+  (:require [spid-docs.sample-responses.defsample :as defsample]
+            [spid-docs.homeless :refer :all]
             [midje.sweet :refer :all]))
 
 (fact
  (eval-in-ns '(str "Hey")) => "Hey"
  (eval-in-ns '(str "Hey") (find-ns 'user)) => "Hey"
  (eval-in-ns 'sample-responses
-             (find-ns 'spid-docs.sample-responses.defsample)) => spid-docs.sample-responses.defsample/sample-responses)
+             (find-ns 'spid-docs.sample-responses.defsample)) => defsample/sample-responses)
