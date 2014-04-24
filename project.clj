637c42cc8d4e34240f0642016ce34d4c58048a4c
Magnar Sveen <magnars@gmail.com>
2014-04-24 09:15:53 +0200

    Increase build process memory allowance.

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  [org.slf4j/slf4j-api "1.7.5"]
                  [ch.qos.logback/logback-classic "1.0.13"]]
   :jvm-opts ["-Djava.awt.headless=true"
-             "-Xmx768M"]
+             "-Xmx1024M"]
   :test2junit-output-dir "target/test2junit"
   :resource-paths ["resources" "generated"]
   :ring {:handler spid-docs.web/app}
