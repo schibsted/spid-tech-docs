96fa06943976a24eb898011d6c3e5111661761e2
Magnar Sveen <magnars@gmail.com>
2014-04-25 09:56:22 +0200

    Remove strict check of API section and api
    - don't want to force an update of validation when a section is added

--- src/spid_docs/validate_cultivated.clj
+++ src/spid_docs/validate_cultivated.clj
@@ -4,31 +4,6 @@
 
 (def Path (pred (fn [^String s] (re-find #"^(/[a-zA-Z0-9/{}_\-.]+)+/?\*?$" s)) 'simple-slash-prefixed-path))
 
-(def APICategory
-  (enum {:section "Identity Management" :api "Login API"}
-        {:section "Identity Management" :api "User API"}
-        {:section "Identity Management" :api "Email API"}
-        {:section "Payment Services"    :api "Product API"}
-        {:section "Payment Services"    :api "Subscription API"}
-        {:section "Payment Services"    :api "Bundle API"}
-        {:section "Payment Services"    :api "Digital Contents API"}
-        {:section "Payment Services"    :api "Payment API"}
-        {:section "Payment Services"    :api "Direct Payment API"}
-        {:section "Payment Services"    :api "Order API"}
-        {:section "Payment Services"    :api "Campaign API"}
-        {:section "Payment Services"    :api "Voucher API"}
-        {:section "Payment Services"    :api "PayLink API"}
-        {:section "Payment Services"    :api "InjectToken API"}
-        {:section "Payment Services"    :api "Discount API"}
-        {:section "Data Storage"        :api "SODA Endpoints"}
-        {:section "Data Storage"        :api "DataObjects API"}
-        {:section "Data Storage"        :api "Traits API"}
-        {:section "Insight"             :api "Report Database API"}
-        {:section "Insight"             :api "KPI API"}
-        {:section "Authorization"       :api "OAuth"}
-        {:section "Utilities"           :api "Platform API"}
-        {:section "Utilities"           :api "Sysadmin API"}))
-
 (def Parameter
   {:name Str
    (optional-key :aliases) [Str]
@@ -57,7 +32,7 @@
    :method (enum :GET :POST :DELETE)
    :name Str
    :description (maybe Str)
-   :category APICategory
+   :category {:section Str, :api Str}
    :parameters [Parameter]
    :response-formats [ResponseFormat]
    :default-response-format ResponseFormat
