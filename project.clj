0783f5a27b4c6e4a9a48cd43ca794c462294a8bc
Magnar Sveen <magnars@gmail.com>
2014-05-05 11:33:45 +0200

    Update Stasis

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "SPiD API endpoint documentation"
   :url "https://payment.schibsted.no"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [stasis "1.0.1"]
+                 [stasis "1.1.1"]
                  [ring "1.2.1"]
                  [optimus "0.14.3"]
                  [hiccup "1.0.5"]
