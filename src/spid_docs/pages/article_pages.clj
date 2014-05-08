dff2ac55c90e3a587ac180d68efe97046728c385
Christian Johansen <christian@cjohansen.no>
2014-05-08 15:28:19 +0200

    Add comments to articles

--- src/spid_docs/pages/article_pages.clj
+++ src/spid_docs/pages/article_pages.clj
@@ -16,7 +16,8 @@
      [:ul (map render-relevant-endpoint relevant)])))
 
 (defn create-page [[_ {:keys [title body aside relevant-endpoints] :as article}]]
-  (let [body (list [:h1 title] (markdown/render body))]
+  (let [body (list [:h1 title] (markdown/render body))
+        comments [:div.disqus-comments {:id (article-path _)}]]
     (if (or aside relevant-endpoints)
       (-> article
           (assoc :split-page? true)
@@ -27,8 +28,9 @@
                           (when aside
                             (markdown/render aside))
                           (when relevant-endpoints
-                            (render-relevant-endpoints relevant-endpoints))]])))
-      (assoc article :body [:div.wrap body]))))
+                            (render-relevant-endpoints relevant-endpoints))]]
+                        comments)))
+      (assoc article :body (list [:div.wrap body] comments)))))
 
 (defn create-pages
   "Given a map of markdown files (path => content), generate a map of url =>
