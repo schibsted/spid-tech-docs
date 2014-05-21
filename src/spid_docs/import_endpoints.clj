0935882e0a7e6e73f17db8db96abc09858f41fbe
Magnar Sveen <magnars@gmail.com>
2014-05-21 11:54:12 +0200

    Import endpoints: Extract comparison to diff-endpoints

--- src/spid_docs/import_endpoints.clj
+++ src/spid_docs/import_endpoints.clj
@@ -1,92 +1,8 @@
 (ns spid-docs.import-endpoints
   (:require [clojure.data.json :as json]
-            [clojure.set :as set]
-            [schema.core :as schema]
             [spid-docs.api-client :refer [GET get-config get-server-token config-exists?]]
             [spid-docs.content :refer [load-content]]
-            [spid-docs.homeless :refer [assoc-if assoc-non-nil]]
-            [spid-docs.validate-raw :refer [Endpoint]]))
-
-(defn- extract-relevant-keys [endpoints]
-  "Return a list of all property names used for all endpoints, including the
-   properties used in all httpMethod objects."
-  (->> (concat (map name (mapcat keys endpoints))
-               (map #(str "httpMethods/" (name %))
-                    (mapcat keys (mapcat (comp vals :httpMethods) endpoints))))
-       set))
-
-(defn- schema-is-valid? [endpoints]
-  "Check if the given endpoints matches our expectations."
-  (not (schema/check [Endpoint] endpoints)))
-
-(defn- find-schema-changes [old new]
-  "Compare properties used to describe endpoints in old and new version."
-  (let [old-keys (extract-relevant-keys old)
-        new-keys (extract-relevant-keys new)
-        added (set/difference new-keys old-keys)
-        removed (set/difference old-keys new-keys)]
-    (when (or (seq added) (seq removed))
-      {:added added, :removed removed})))
-
-(defn- get-signatures [endpoint]
-  "Return a list of {:path :method} for each of httpMethods in endpoint."
-  (map (fn [method] {:path (:path endpoint)
-                     :method method})
-       (keys (:httpMethods endpoint))))
-
-(defn- flatten-http-methods [endpoint]
-  "Return a list of endpoints each with a single entry in httpMethods from the original."
-  (let [methods (:httpMethods endpoint)]
-    (map (fn [method] (assoc endpoint :httpMethods (select-keys methods [method])))
-         (keys methods))))
-
-(defn- find-endpoint-changes [old new]
-  "Return a map of added, removed and changed endpoints."
-  (let [old-signatures (set (mapcat get-signatures old))
-        new-signatures (set (mapcat get-signatures new))
-        added (set/difference new-signatures old-signatures)
-        removed (set/difference old-signatures new-signatures)
-        changed (-> (set (mapcat get-signatures
-                                 (set/difference (set (mapcat flatten-http-methods old))
-                                                 (set (mapcat flatten-http-methods new)))))
-                    (set/difference added removed))]
-    (cond-> {}
-            (seq added) (assoc :added added)
-            (seq removed) (assoc :removed removed)
-            (seq changed) (assoc :changed changed))))
-
-(defn- all-params [endpoint]
-  (concat (:pathParameters endpoint)
-          (mapcat :optional (vals (:httpMethods endpoint)))
-          (mapcat :required (vals (:httpMethods endpoint)))))
-
-(defn- find-param-changes [old new]
-  "Return a map of added and removed parameter names."
-  (let [old-params (set (mapcat all-params old))
-        new-params (set (mapcat all-params new))
-        added (set/difference new-params old-params)
-        removed (set/difference old-params new-params)]
-    (when (or (seq added) (seq removed))
-      (-> {}
-          (assoc-non-nil :added added)
-          (assoc-non-nil :removed removed)))))
-
-(defn- add-schema-changes [m old new]
-  "If there are any schema changes, add them to the map along with :schema-change set to true."
-  (if-let [schema-changes (find-schema-changes old new)]
-    (assoc m
-      :schema-change? true
-      :schema schema-changes)
-    m))
-
-(defn compare-endpoint-lists [old new]
-  "Diff old and new endpoints, and return a summary of the changes."
-  (when-not (= old new)
-    (-> {}
-        (add-schema-changes old new)
-        (assoc-if (not (schema-is-valid? new)) :breaking-change? true)
-        (assoc-non-nil :params (find-param-changes old new))
-        (merge (find-endpoint-changes old new)))))
+            [spid-docs.diff-endpoints :refer [diff-endpoints]]))
 
 (def import-path "/endpoints")
 
@@ -116,12 +32,12 @@
     (let [missing (remove example-params (-> diff :params :added))
           superflous (filter example-params (-> diff :params :removed))]
       (when (seq missing)
-        (println "Examples are missing for these new parameters,")
-        (println "and should be added to resources/example-params.edn:")
+        (println "NB! Examples are missing for these new parameters,")
+        (println "and must be added to resources/example-params.edn:")
         (doseq [p missing] (println "  -" p))
         (println))
       (when (seq superflous)
-        (println "Consider removing these removed parameters from resources/example-params.edn:")
+        (println "Consider removing these outdated parameters from resources/example-params.edn:")
         (doseq [p superflous] (println "  -" p))
         (println)))))
 
@@ -145,7 +61,7 @@
       (println))
     (let [old-content (load-content)
           {:keys [new-endpoints raw-response]} (load-new-endpoints)
-          diff (compare-endpoint-lists (:endpoints old-content) new-endpoints)]
+          diff (diff-endpoints (:endpoints old-content) new-endpoints)]
       (if-not diff
         (println "No changes detected.")
         (if (:breaking-change? diff)
---
+++ test/spid_docs/diff_endpoints_test.clj
@@ -0,0 +1,92 @@
+(ns spid-docs.diff-endpoints-test
+  (:require [midje.sweet :refer :all]
+            [spid-docs.cultivate.content-shells :as cs]
+            [spid-docs.diff-endpoints :refer :all]))
+
+(fact
+ "Equal lists are equal. No changes detected. Returns nil."
+
+ (diff-endpoints [] []) => nil
+ (diff-endpoints [(cs/endpoint)] [(cs/endpoint)]) => nil)
+
+(fact
+ "The properties that describe each endpoint (the schema) are checked for
+  changes."
+
+ (-> (diff-endpoints [(-> (cs/endpoint)
+                               (dissoc :url)
+                               (assoc :urls []))]
+                          [(cs/endpoint)])
+     (dissoc :changed)) ;; not relevant for this test
+ => {:schema-change? true
+     :schema {:added #{"url"}
+              :removed #{"urls"}}})
+
+(fact
+ "We also check the properties used to describe httpMethods."
+
+ (-> (diff-endpoints [(cs/endpoint
+                            {:httpMethods {:GET (-> (cs/http-method)
+                                                    (dissoc :name)
+                                                    (assoc :id "x"))}})]
+                          [(cs/endpoint
+                            {:httpMethods {:GET (cs/http-method)}})])
+     (dissoc :changed)) ;; not relevant for this test
+ => {:schema-change? true
+     :schema {:removed #{"httpMethods/id"}
+              :added #{"httpMethods/name"}}})
+
+(fact
+ "The new endpoints are validated, to see if the changes are breaking,
+  ie. no longer matches our expectations."
+
+ (-> (diff-endpoints [(cs/endpoint)]
+                          [(-> (cs/endpoint)
+                               (dissoc :url)
+                               (assoc :urls []))])
+     (dissoc :changed)) ;; not relevant for this test
+ => {:schema-change? true
+     :schema {:removed #{"url"}
+              :added #{"urls"}}
+     :breaking-change? true})
+
+(fact
+ "We detect added and removed endpoints, determined by [path method] pairs."
+
+ (diff-endpoints [(cs/endpoint
+                        {:path "/path"
+                         :httpMethods {:GET (cs/http-method)}})]
+                      [(cs/endpoint
+                        {:path "/path"
+                         :httpMethods {:POST (cs/http-method)}})])
+ => {:added #{{:path "/path", :method :POST}}
+     :removed #{{:path "/path", :method :GET}}})
+
+(fact
+ "We also detect any changes to existing endpoints, but don't report on
+  exactly what these changes are."
+
+ (diff-endpoints [(cs/endpoint
+                        {:path "/path"
+                         :httpMethods {:GET (cs/http-method
+                                             {:name "abc"})}})]
+                      [(cs/endpoint
+                        {:path "/path"
+                         :httpMethods {:GET (cs/http-method
+                                             {:name "def"})}})])
+ => {:changed #{{:path "/path", :method :GET}}})
+
+(fact
+ "We detect changes in parameter names."
+
+ (-> (diff-endpoints [(cs/endpoint {:pathParameters ["old-pp" "same-pp"]
+                                         :httpMethods {:GET (cs/http-method
+                                                             {:optional ["old-opt" "same-opt"]
+                                                              :required ["old-req" "same-req"]})}})]
+                          [(cs/endpoint {:pathParameters ["new-pp" "same-pp"]
+                                         :httpMethods {:GET (cs/http-method
+                                                             {:optional ["new-opt" "same-opt"]
+                                                              :required ["new-req" "same-req"]})}})])
+     :params)
+ => {:removed #{"old-opt" "old-pp" "old-req"}
+     :added #{"new-opt" "new-pp" "new-req"}})
