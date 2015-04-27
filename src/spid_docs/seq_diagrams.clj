ab9ef729765a630a5f3a353a0d4bded8f7aaaaac
Joakim Wånggren <joakim.wanggren@schibsted.com>
2015-04-27 16:09:09 +0200

    Added npm install to build

--- src/spid_docs/seq_diagrams.clj
+++ src/spid_docs/seq_diagrams.clj
@@ -3,6 +3,14 @@
             [clojure.java.shell :refer [sh]]
             [digest :refer [md5]]))
 
+(def npm-install
+  "Install npm dependencies" (delay
+    (let [result (sh "npm" "install"
+                     :dir "seq-diagrams/")]
+      (if (= 0 (:exit result))
+        (:out result)
+        (throw (ex-info "Could not npm install" result))))))
+
 (defn generate-svg
   "Shell out to the sequence diagram generator in node, passing the
    definition through standard in."
@@ -17,6 +25,7 @@
 (defn get-sequence-diagram [definition]
   "Hash the definition to generate a unique path, and either return the
    previously generated svg, or generate it now, save and return."
+   @npm-install
   (let [path (str "sequence-diagrams/" (md5 definition) ".svg")]
     {:path (str "/" path)
      :svg (if-let [cached (io/resource path)]
